e192fd3956ffe5049885925c0955f00d20f9d440
John Kelvie <john.kelvie@gmail.com>
2017-11-28 21:30:28 -0500

    Added circle file for auto-deployment

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Guess The Price
+Showcase for Bespoken Unit-Testing and Integration-Testing tools!
+Also, fun game for guessing prices, akin to Price Is Right!
+
+##
---
+++ test/integration-test.js
@@ -0,0 +1,35 @@
+require("babel-polyfill");
+require("dotenv").config();
+const assert = require("chai").assert;
+const vds = require("virtual-device-sdk");
+
+describe("GuessThePrice Integration Test", function() {
+    this.timeout(120 * 1000);
+
+    describe("Onboarding Test", function () {
+        it("Initial Test successfully", async () => {
+            const virtualDevice = new vds.VirtualDevice(process.env.VIRTUAL_DEVICE_TOKEN);
+            await virtualDevice.message("alexa exit");
+            let result = await virtualDevice.message("open guess the price");
+
+            assert.include(result.transcript, "welcome to guess the price how many persons are playing today");
+
+            result = await virtualDevice.message("two");
+            console.log("Result: " + result.transcript);
+            assert.include(result.transcript, "what is your name");
+
+            result = await virtualDevice.message("john");
+            console.log("Result: " + result.transcript);
+            assert.include(result.transcript, "tell us what is your name");
+
+            result = await virtualDevice.message("frank");
+            console.log("Result: " + result.transcript);
+            assert.include(result.transcript, "start the game");
+            assert.include(result.transcript, "your product is");
+
+            result = await virtualDevice.message("200 dollars");
+            console.log("Result: " + result.transcript);
+            assert.include(result.transcript, "the actual price was");
+        });
+    });
+});
