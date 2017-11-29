77fd3835f411c2ed5d4d63509017d585d1193828
John Kelvie <john.kelvie@gmail.com>
2017-11-28 22:28:48 -0500

    Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+echo "ENV: $1"
+
+# Create the .aws and .ask directories
+mkdir ~/.aws
+mkdir ~/.ask
+
+# Create AWS credentials
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+
+# Create ASK config
+sed -e s/ASK_ACCESS_TOKEN/${ASK_ACCESS_TOKEN}/g -e \
+    s/ASK_REFRESH_TOKEN/${ASK_REFRESH_TOKEN}/g conf/ask_cli.json > ~/.ask/cli_config
+
+# Create the Lambda upload file and deploy it
+npm run package
+ask lambda upload -f GuessThePrice-Dev -s artifact.zip
